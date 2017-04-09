b1cd970548a1cb41464871f6b5bbbfe16d532341
thelazyindian <satyabrat.me@gmail.com>
2017-04-09 18:16:51 +0530

    Update UI

--- app/src/main/java/feedback/romil/com/feedbackdesign/MainActivity.java
+++ app/src/main/java/feedback/romil/com/feedbackdesign/MainActivity.java
@@ -3,6 +3,7 @@ package feedback.romil.com.feedbackdesign;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
     public boolean emailValidator(String email)
     {
