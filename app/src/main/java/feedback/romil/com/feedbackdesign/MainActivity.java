87c47c6b0b17d67ef4e4c6301884e46ea10863e2
Romil <romilparhwal007@gmail.com>
2017-04-09 15:44:57 +0530

    Initial Commit

---
+++ app/src/main/java/feedback/romil/com/feedbackdesign/MainActivity.java
@@ -0,0 +1,80 @@
+package feedback.romil.com.feedbackdesign;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    int fail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean emailValidator(String email)
+    {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public void sendFeedback(View button) {
+        // Do click handling here
+        fail=0;
+        final EditText nameField = (EditText) findViewById(R.id.EditTextName);
+        String name = nameField.getText().toString();
+        if(name.matches("")){
+            fail=1;
+            Context context = getApplicationContext();
+            CharSequence text = "Name is not provided";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        final EditText emailField = (EditText) findViewById(R.id.editText);
+        String email = emailField.getText().toString();
+        if(emailValidator(email)==false){
+            fail=1;
+            Context context = getApplicationContext();
+            CharSequence text = "E-Mail Not Valid";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);
+        String feedback = feedbackField.getText().toString();
+        if(feedback.matches("")){
+            fail=1;
+            Context context = getApplicationContext();
+            CharSequence text = "No Feedback Message";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);
+        String feedbackType = feedbackSpinner.getSelectedItem().toString();
+
+        Context context = getApplicationContext();
+        CharSequence text = "Feedback Sent!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+//        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM, 0, 0);
+        if(fail==0) {
+            toast.show();
+        }
+    }
+}
